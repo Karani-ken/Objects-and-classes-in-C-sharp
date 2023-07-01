133c02cb0673f549b0a2a372a8f9a1911b59d639
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-02 01:07:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_classes_in_C_
+{
+    public class Animal
+    {
+       public string name;
+       public string family;
+       public string type;
+       public string sound;
+
+        public void makeSound()
+        {
+            Console.WriteLine($"This animal is called a {name} and" +
+                $" it is a {family}. \n It is a {type} animal and makes a {sound} sound");
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal();
+
+            cow.name = "Cow";
+            cow.family = "Mammal";
+            cow.type = "Dairy";
+            cow.sound = "Moooh";
+
+            cow.makeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
