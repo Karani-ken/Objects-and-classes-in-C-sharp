09b5ef6843b2809c9abba084214ffa12871cda38
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-02 10:37:50 +0300

    created namespace

---
+++ Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Objects_and_classes_in_C_.Animals
+{
+    public class Animal
+    {
+        public string name;
+        public string family;
+        public string type;
+        public string sound;
+
+        public void makeSound()
+        {
+            Console.WriteLine($"This animal is called a {name} and" +
+                $" it is a {family}. \n It is a {type} animal and makes a {sound} sound");
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,25 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Objects_and_classes_in_C_.Animals;
 namespace Objects_and_classes_in_C_
 {
-    public class Animal
-    {
-       public string name;
-       public string family;
-       public string type;
-       public string sound;
-
-        public void makeSound()
-        {
-            Console.WriteLine($"This animal is called a {name} and" +
-                $" it is a {family}. \n It is a {type} animal and makes a {sound} sound");
-        }
-
-
-
-    }
+   
     internal class Program
     {
         static void Main(string[] args)
